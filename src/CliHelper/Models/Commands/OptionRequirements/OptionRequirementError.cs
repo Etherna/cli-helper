40615c75760551c8899eeb8a776f53a103661ce3
Mirko Da Corte <mirko@etherna.io>
2024-07-02 20:56:08 +0200

    update license headers

--- src/CliHelper/Models/Commands/OptionRequirements/OptionRequirementError.cs
+++ src/CliHelper/Models/Commands/OptionRequirements/OptionRequirementError.cs
@@ -1,15 +1,15 @@
 // Copyright 2024-present Etherna SA
-// This file is part of Etherna Gateway CLI.
+// This file is part of Cli Helper.
 // 
-// Etherna Gateway CLI is free software: you can redistribute it and/or modify it under the terms of the
-// GNU Affero General Public License as published by the Free Software Foundation,
+// Cli Helper is free software: you can redistribute it and/or modify it under the terms of the
+// GNU Lesser General Public License as published by the Free Software Foundation,
 // either version 3 of the License, or (at your option) any later version.
 // 
-// Etherna Gateway CLI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// Cli Helper is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 // without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-// See the GNU Affero General Public License for more details.
+// See the GNU Lesser General Public License for more details.
 // 
-// You should have received a copy of the GNU Affero General Public License along with Etherna Gateway CLI.
+// You should have received a copy of the GNU Lesser General Public License along with Cli Helper.
 // If not, see <https://www.gnu.org/licenses/>.
 
 namespace Etherna.CliHelper.Models.Commands.OptionRequirements
